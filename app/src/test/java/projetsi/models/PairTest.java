02135672e06a314baf43b316c50ac3ea962dcefd
Baptiste Asselin <baptiste.asselin@orange.fr>
2023-10-13 18:01:40 +0200

    BAS #9 created interface for spot file parser result

---
+++ app/src/test/java/projetsi/models/PairTest.java
@@ -0,0 +1,21 @@
+package projetsi.models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PairTest {
+    
+    @Test void pairGetFirstAndSecond() {
+        Pair<String, Integer> classUnderTest = new Pair<>("first", 2);
+        assertNotNull(classUnderTest.getFirst(), "getFirst should return the first element");
+        assertNotNull(classUnderTest.getSecond(), "getSecond should return the second element");
+    }
+
+    @Test void pairSetFirstAndSecond() {
+        Pair<String, Integer> classUnderTest = new Pair<>("first", 2);
+        classUnderTest.setFirst("first");
+        classUnderTest.setSecond(2);
+        assertNotNull(classUnderTest.getFirst(), "getFirst should return the first element");
+        assertNotNull(classUnderTest.getSecond(), "getSecond should return the second element");
+    }
+}
