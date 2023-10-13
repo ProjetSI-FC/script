380339af43eadb0e77148d9ee3ee2d0360f0517b
Baptiste Asselin <baptiste.asselin@orange.fr>
2023-10-13 18:19:03 +0200

    BAS #9 created interface for keywords permutations computation

---
+++ app/src/main/java/projetsi/interfaces/SpotfileKeywordsPermutations.java
@@ -0,0 +1,17 @@
+package projetsi.interfaces;
+
+import java.util.Map;
+import java.util.SortedSet;
+
+import projetsi.models.Pair;
+
+/**
+ * Interface class that must be implemented by classes that will compute the permutations of keywords and the associated score for a spot file
+ */
+public interface SpotfileKeywordsPermutations extends Iterable<Pair<SortedSet<String>, Integer>> {
+    
+    /**
+     * @return A dictionary of the spot file metadatas (transcript file path, audio file path, video file path, xml file path, occurence score, etc...)
+     */
+    Map<String, String> getSpotFileMetadatas();
+}
